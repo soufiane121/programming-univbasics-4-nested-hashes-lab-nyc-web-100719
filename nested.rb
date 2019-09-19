5b11a18cd4769ea930a01046c596f4fd972f9bb1
soufiane oucherrou <soufiane.oucherrou@gmail.com>
2019-09-19 20:46:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-    p programmer_hash[:alan_kay][:known_for]= "GUI"
+    alana_know_for= programmer_hash[:alan_kay][:known_for]= "GUI"
+    alana_know_for
 end
 
 def adding_to_dennis
