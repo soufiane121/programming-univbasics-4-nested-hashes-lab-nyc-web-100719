bcc86634bc3e16c31a60033e88918da99b54fbc4
soufiane oucherrou <soufiane.oucherrou@gmail.com>
2019-09-19 20:41:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,9 @@ def adding_matz
         }
      }
 
-    programmer_hash["keys"]= ""
+    programmer_hash[:yukihiro_matsumoto]= {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
     programmer_hash
 end
 
